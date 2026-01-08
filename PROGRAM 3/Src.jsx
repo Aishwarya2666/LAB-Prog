fd65ddbd5b9c62b62f8908ac6fdba1e536d8ada6
Aishwarya S <aishwaryaa0263@gmail.com>
2026-01-09 00:42:23 +0530

    Update Src.jsx

--- PROGRAM 3/Src.jsx
+++ PROGRAM 3/Src.jsx
@@ -1,70 +1,51 @@
-import { useState } from "react";
-function App() {
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [submittedData, setSubmittedData] = useState(null);
+import React, { useState } from 'react'
 
-const handleSubmit = (e) => {
-e.preventDefault();
-const data = { name, email, password };
-setSubmittedData(data);
+function Forms() {
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const [submittedData,setSubmittedData]=useState(null)
 
-setName("");
 
-setEmail("");
-setPassword("");
-};
-
-return (
-<div className="card">
-<h1>Registration Form</h1>
-
-<form onSubmit={handleSubmit}>
-<label>Name</label>
-<input
-type="text"
-className="input-box"
-value={name}
-onChange={(e) => setName(e.target.value)}
-required
-/>
-
-<label>Email</label>
-<input
-type="email"
-className="input-box"
-value={email}
-
-onChange={(e) => setEmail(e.target.value)}
-required
-/>
-
-<label>Password</label>
-<input
-type="password"
-className="input-box"
-value={password}
-onChange={(e) => setPassword(e.target.value)}
-required
-/>
-
-<button type="submit" className="btn">
-Submit
-</button>
-</form>
-
-{submittedData && (
-<div className="output-card">
-<h2>Submitted Data</h2>
-<p><strong>Name:</strong> {submittedData.name}</p>
-<p><strong>Email:</strong> {submittedData.email}</p>
-
-<p><strong>Password:</strong> {submittedData.password}</p>
-</div>
-)}
-</div>
-);
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const formdata={name,email,password}
+        setSubmittedData(formdata);
+        setName("")
+        setEmail("")
+        setPassword("")
+    }
+  return (
+    <div>
+        <h2>Forms</h2>
+        <form onSubmit={handleSubmit}>
+            <label>Name:</label>
+            <input type="text" 
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+            /><br></br>
+            <label>Email:</label>
+            <input type="email" 
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            /><br></br>
+            <label>Password:</label>
+            <input type="password" 
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            />
+            <button type="submit">Submit</button>
+        </form>
+        {submittedData &&(
+            <div>
+                <p><strong>Name:</strong>{submittedData.name}</p>
+                <p><strong>Email:</strong>{submittedData.email}</p>
+                <p><strong>Password:</strong>{submittedData.password}</p>
+            </div>
+        )}
+     
+    </div>
+  )
 }
 
-export default App;
+export default Forms
